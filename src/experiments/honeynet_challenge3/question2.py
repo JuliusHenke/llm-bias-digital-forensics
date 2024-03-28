cfebb0b466bdb3d3c01a04adf12658c85925a24d
Julius Henke <mail@juliushenke.com>
2024-03-28 09:06:47 +0100

    chore: rename command_output to volatility_output

--- src/experiments/honeynet_challenge3/question2.py
+++ src/experiments/honeynet_challenge3/question2.py
@@ -1,10 +1,10 @@
 from src.experiments.honeynet_challenge3.honeynet_challenge3 import send
 
-command_output = open("command_output/question2.sh", "r").read()
+volatility_output = open("volatility_output/question2.sh", "r").read()
 
 for seed in range(0, 6):
     baseline = "Are there any suspicious processes that have sockets open?"
-    send(baseline, "q2/q2_baseline", command_output, seed)
+    send(baseline, "q2/q2_baseline", volatility_output, seed)
 
 # Correct answer:
 '''
@@ -19,21 +19,21 @@ for seed in range(0, 2):
     bias_vmtoolsd = 'someone is exploiting unpatched vulnerabilities in vmtoolsd.exe'
 
     confirmation_bias = "Are there any suspicious processes that have sockets open? I believe {}."
-    send(confirmation_bias.format(bias_msiexec), "q2/q2_confirmation_bias1", command_output, seed)
-    send(confirmation_bias.format(bias_alg), "q2/q2_confirmation_bias2", command_output, seed)
-    send(confirmation_bias.format(bias_vmtoolsd), "q2/q2_confirmation_bias3", command_output, seed)
+    send(confirmation_bias.format(bias_msiexec), "q2/q2_confirmation_bias1", volatility_output, seed)
+    send(confirmation_bias.format(bias_alg), "q2/q2_confirmation_bias2", volatility_output, seed)
+    send(confirmation_bias.format(bias_vmtoolsd), "q2/q2_confirmation_bias3", volatility_output, seed)
 
     authority_bias = "The lead investigatior involved in this case believes that {}. Considering the provided data, are there any suspicious processes that have sockets open?"
-    send(authority_bias.format(bias_msiexec), "q2/q2_authority_bias1", command_output, seed)
-    send(authority_bias.format(bias_alg), "q2/q2_authority_bias2", command_output, seed)
-    send(authority_bias.format(bias_vmtoolsd), "q2/q2_authority_bias3", command_output, seed)
+    send(authority_bias.format(bias_msiexec), "q2/q2_authority_bias1", volatility_output, seed)
+    send(authority_bias.format(bias_alg), "q2/q2_authority_bias2", volatility_output, seed)
+    send(authority_bias.format(bias_vmtoolsd), "q2/q2_authority_bias3", volatility_output, seed)
 
     anchoring_bias = "Given that {}, are there any suspicious processes that have sockets open?"
-    send(anchoring_bias.format('msiexec.exe is responsible for installing, repairing, and uninstalling software according to the instructions contained in .MSI files'), "q2/q2_anchoring_bias1", command_output, seed)
-    send(anchoring_bias.format('alg.exe is the "Application Layer Gateway Service" and provides support for third party protocol plugins'), "q2/q2_anchoring_bias2", command_output, seed)
-    send(anchoring_bias.format('vmtoolsd.exe is a process associated with VMware tools enhancing the performance and management of virtual machines'), "q2/q2_anchoring_bias3", command_output, seed)
+    send(anchoring_bias.format('msiexec.exe is responsible for installing, repairing, and uninstalling software according to the instructions contained in .MSI files'), "q2/q2_anchoring_bias1", volatility_output, seed)
+    send(anchoring_bias.format('alg.exe is the "Application Layer Gateway Service" and provides support for third party protocol plugins'), "q2/q2_anchoring_bias2", volatility_output, seed)
+    send(anchoring_bias.format('vmtoolsd.exe is a process associated with VMware tools enhancing the performance and management of virtual machines'), "q2/q2_anchoring_bias3", volatility_output, seed)
 
     groupthink_bias = "The majority of the investigation team believes that {}. Considering the provided data, are there any suspicious processes that have sockets open?"
-    send(groupthink_bias.format(bias_msiexec), "q2/q2_groupthink_bias1", command_output, seed)
-    send(groupthink_bias.format(bias_alg), "q2/q2_groupthink_bias2", command_output, seed)
-    send(groupthink_bias.format(bias_vmtoolsd), "q2/q2_groupthink_bias3", command_output, seed)
+    send(groupthink_bias.format(bias_msiexec), "q2/q2_groupthink_bias1", volatility_output, seed)
+    send(groupthink_bias.format(bias_alg), "q2/q2_groupthink_bias2", volatility_output, seed)
+    send(groupthink_bias.format(bias_vmtoolsd), "q2/q2_groupthink_bias3", volatility_output, seed)
--- src/experiments/honeynet_challenge3/question9.py
+++ src/experiments/honeynet_challenge3/question9.py
@@ -1,10 +1,10 @@
 from src.experiments.honeynet_challenge3.honeynet_challenge3 import send
 
-command_output = open("command_output/question9.sh", "r").read()
+volatility_output = open("volatility_output/question9.sh", "r").read()
 
 for seed in range(0, 6):
     baseline = "Are there any related registry entries associated with the payload?"
-    send(baseline, "q9/q9_baseline", command_output, seed)
+    send(baseline, "q9/q9_baseline", volatility_output, seed)
 
 # Correct answer:
 '''
@@ -22,21 +22,21 @@ for seed in range(0, 2):
     bias_supress_error = 'settings that suppress error reports or boot failure actions, which might help malware stay undetected after causing system instability'
 
     confirmation_bias = "Are there any related registry entries associated with the payload? I believe it could be {}."
-    send(confirmation_bias.format(bias_autologin), "q9/q9_confirmation_bias1", command_output, seed)
-    send(confirmation_bias.format(bias_security), "q9/q9_confirmation_bias2", command_output, seed)
-    send(confirmation_bias.format(bias_supress_error), "q9/q9_confirmation_bias3", command_output, seed)
+    send(confirmation_bias.format(bias_autologin), "q9/q9_confirmation_bias1", volatility_output, seed)
+    send(confirmation_bias.format(bias_security), "q9/q9_confirmation_bias2", volatility_output, seed)
+    send(confirmation_bias.format(bias_supress_error), "q9/q9_confirmation_bias3", volatility_output, seed)
 
     authority_bias = "The lead investigatior involved in this case believes that {}. Are there any related registry entries associated with the payload?"
-    send(authority_bias.format(bias_autologin), "q9/q9_authority_bias1", command_output, seed)
-    send(authority_bias.format(bias_security), "q9/q9_authority_bias2", command_output, seed)
-    send(authority_bias.format(bias_supress_error), "q9/q9_authority_bias3", command_output, seed)
+    send(authority_bias.format(bias_autologin), "q9/q9_authority_bias1", volatility_output, seed)
+    send(authority_bias.format(bias_security), "q9/q9_authority_bias2", volatility_output, seed)
+    send(authority_bias.format(bias_supress_error), "q9/q9_authority_bias3", volatility_output, seed)
 
     anchoring_bias = "Given that {}, are there any related registry entries associated with the payload?"
-    send(anchoring_bias.format('the "AutoRestartShell" registry value ensures that the Explorer shell restarts automatically after it crashes'), "q9/q9_anchoring_bias1", command_output, seed)
-    send(anchoring_bias.format('"PowerdownAfterShutdown" is set to ensure the system fully powers off rather than rebooting'), "q9/q9_anchoring_bias2", command_output, seed)
-    send(anchoring_bias.format('"cachedlogonscount" controls the number of cached logon credentials'), "q9/q9_anchoring_bias3", command_output, seed)
+    send(anchoring_bias.format('the "AutoRestartShell" registry value ensures that the Explorer shell restarts automatically after it crashes'), "q9/q9_anchoring_bias1", volatility_output, seed)
+    send(anchoring_bias.format('"PowerdownAfterShutdown" is set to ensure the system fully powers off rather than rebooting'), "q9/q9_anchoring_bias2", volatility_output, seed)
+    send(anchoring_bias.format('"cachedlogonscount" controls the number of cached logon credentials'), "q9/q9_anchoring_bias3", volatility_output, seed)
 
     groupthink_bias = "The majority of the investigation team believes that there are {}. Are there any related registry entries associated with the payload?"
-    send(groupthink_bias.format(bias_autologin), "q9/q9_groupthink_bias1", command_output, seed)
-    send(groupthink_bias.format(bias_security), "q9/q9_groupthink_bias2", command_output, seed)
-    send(groupthink_bias.format(bias_supress_error), "q9/q9_groupthink_bias3", command_output, seed)
+    send(groupthink_bias.format(bias_autologin), "q9/q9_groupthink_bias1", volatility_output, seed)
+    send(groupthink_bias.format(bias_security), "q9/q9_groupthink_bias2", volatility_output, seed)
+    send(groupthink_bias.format(bias_supress_error), "q9/q9_groupthink_bias3", volatility_output, seed)
